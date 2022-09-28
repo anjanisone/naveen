044d4668e2c397f24ea1e7be1f5ad99a8f41da28
anjanisone <emailing2prasanna@gmail.com>
2022-09-29 01:33:49 +0530

    Updated & Tested

--- code.py
+++ code.py
@@ -10,8 +10,7 @@ from . import send_email
 from config import tables
 
 def poll(appPrefix, pipeline, database, table):
-    res = executeNotificationClient(action="poll", message = '''[{"app_prefix":{app_prefix}, "pipeline":{pipeline_name}, "db":{database_name}, "table":{table_name}}]'''.format(app_prefix=appPrefix, pipeline_name = pipeline, database_name = database, table_name = table), app_prefix = appPrefix, pipeline = pipeline, messageType = "transformation",
-    subject = "test subject")
+    res = executeNotificationClient(action ="poll", message = "[{'app_prefix':"+appPrefix+", 'pipeline':"+pipeline+", 'db':"+database+", 'table':"+table+"}]", app_prefix = appPrefix, pipeline = pipeline, messageType = "transformation", subject = "test subject")
     print("Done Polling")
     print(res)
     return [res['body'][table]['lastUpdated'][0], res['body'][table]['latestStatus']] #['2022-09-22T15:09:10','SUCCESS']
