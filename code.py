6d9be905728dfe7a58c0cb9253e1dc27cd7145db
anjanisone <emailing2prasanna@gmail.com>
2022-09-28 23:03:51 +0530

    Updated at 23.03 IST

--- code.py
+++ code.py
@@ -9,26 +9,25 @@ from . import send_email
 
 from config import tables
 
-def poll(app_prefix, pipeline_name, database_name, table_name):
-    res = executeNotificationClient(action="poll", message = f'''[{"app_prefix":{app_prefix}, "pipeline":{pipeline_name}, "db":{database_name}, "table":{table_name}}]''', app_prefix = app_prefix, pipeline = pipeline_name, messageType = "transformation",
+def poll(appPrefix, pipeline, database, table):
+    res = executeNotificationClient(action="poll", message = '''[{"app_prefix":{app_prefix}, "pipeline":{pipeline_name}, "db":{database_name}, "table":{table_name}}]'''.format(app_prefix=appPrefix, pipeline_name = pipeline, database_name = database, table_name = table), app_prefix = appPrefix, pipeline = pipeline, messageType = "transformation",
     subject = "test subject")
     print("Done Polling")
     print(res)
-    return [res['body'][table_name]['lastUpdated'][0], res['body'][table_name]['latestStatus']] #['2022-09-22T15:09:10','SUCCESS']
+    return [res['body'][table]['lastUpdated'][0], res['body'][table]['latestStatus']] #['2022-09-22T15:09:10','SUCCESS']
 
 
 if __name__ == "__main__":
     
     success = []
     failure = []
-    for key in tables.keys(): #10
-        result = poll(tables[key]['app_prefix'], tables[key]['pipeline'], tables[key]['Database'], tables[key]['database_name'])
+    for key in tables.keys():
+        result = poll(tables[key]['app_prefix'], tables[key]['pipeline'], tables[key]['Database'], key)
         if result[1] == "SUCCESS":
             success.append(result)
         else:
             failure.append(result)
     if len(failure) == 0:
-        #run three files
+        #Run your Three Files Here
     else:
-        pass
         #send email code. 
